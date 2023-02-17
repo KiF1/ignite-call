d26ad810e882efb0e0ad967a1058040fe770da63
Abner Santos <abner.kif1@gmail.com>
2023-02-17 11:38:19 -0300

    add credentials google

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import { z } from 'zod';
-import { buildNexthAuthOptions } from '../auth/[...nextauth].api';
 import { prisma } from '../../../lib/prisma';
+import { buildNextAuthOptions } from '../auth/[...nextauth].api';
 
 const timeIntervalsBodySchema = z.object({
   intervals: z.array(z.object({
@@ -16,7 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if(req.method !== 'POST'){
     return res.status(405).end()
   }
-  const session = await unstable_getServerSession(req, res, buildNexthAuthOptions(req, res));
+  const session = await unstable_getServerSession(req, res, buildNextAuthOptions(req, res));
   
   if(!session){
     return res.status(401).end()
